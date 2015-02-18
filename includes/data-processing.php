256bc8a2e2093ccaaacc2cebe3b158474f9b4e94
Robert Wilde <robwilde@everythingtechnical.com.au>
2015-02-18 21:44:32 +1000

    Update data-processing.php
    Added teh _log function here

--- includes/data-processing.php
+++ includes/data-processing.php
@@ -11,3 +11,17 @@
  * =======================================================
  */
 
+/**
+ * Error Logging
+ */
+if ( ! function_exists( '_log' ) ) {
+	function _log( $message ) {
+		if ( WP_DEBUG === TRUE ) {
+			if ( is_array( $message ) || is_object( $message ) ) {
+				error_log( print_r( $message, TRUE ) );
+			} else {
+				error_log( $message );
+			}
+		}
+	}
+}
