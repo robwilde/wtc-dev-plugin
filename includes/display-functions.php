3be905fc95aeeb4fd64c44da20925ac7dd568827
Robert Wilde <robwilde@everythingtechnical.com.au>
2015-02-18 21:43:19 +1000

    Update display-functions.php
    Added section for _log function

--- includes/display-functions.php
+++ includes/display-functions.php
@@ -60,3 +60,18 @@ function wtc_file_path( $full_file_path ){
 
     echo $theme_name .' - ' . $relative_file_path;
 }
+
+/**
+ * Error Logging
+ */
+if ( ! function_exists( '_log' ) ) {
+	function _log( $message ) {
+		if ( WP_DEBUG === TRUE ) {
+			if ( is_array( $message ) || is_object( $message ) ) {
+				error_log( print_r( $message, TRUE ) );
+			} else {
+				error_log( $message );
+			}
+		}
+	}
+}
